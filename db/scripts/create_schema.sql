f3545ff0a23c1bd0fcc2f041b01089a2b445b490
blavalleelavallee@gmail.com <blavalleelavallee@gmail.com>
2016-09-27 08:24:13 -0400

    + db stock
    --HG--
    branch : first_draft

--- db/scripts/create_schema.sql
+++ db/scripts/create_schema.sql
@@ -11,3 +11,15 @@ CREATE TABLE IF NOT EXISTS upc (
 INSERT IGNORE INTO upc (number, description, size_weight, source) VALUES ('0060410074978','32i0 grams Tostitos Tortilla Chip - Bite Size Rounds - Premium White Corn','320g','upc_db');
 INSERT IGNORE INTO upc (number, description, size_weight, source) VALUES ('0069000014257','355ml/12fl.oz. Canadian Diet Pepsi','355ml','upc_db');
 
+CREATE TABLE IF NOT EXISTS stock (
+        upc VARCHAR(50) NOT NULL PRIMARY KEY,
+        description VARCHAR(500) NOT NULL,
+        expiration DATE NOT NULL,
+        date_achat DATE NOT NULL,
+        CONSTRAINT `fk_new_table_1`
+        FOREIGN KEY (`upc`)
+        REFERENCES `inventaire`.`upc` (`number`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+        );
+
